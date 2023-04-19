20a3cb81057ff8caef119e7fafba713ad4030828
Mauro Simoes <maurosimoes9@gmail.com>
2023-04-19 23:51:46 +0200

    user form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/user', name: 'app_home')]
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
+        $user = new User();
+
+        $form = $this->createForm(UserFormType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $user = $form->getData();
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'the home controller',
+            'form' => $form,
         ]);
     }
 }
